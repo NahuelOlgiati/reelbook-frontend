f2462e72d5ba2b4eb378a3634191779897621d3a
NahuelOlgiati <nahuelolgiati@hotmail.com>
2016-09-30 16:24:21 -0300

    wall

---
+++ src/app/document-type/document-type-wall/document-type-wall.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { DocumentType } from '../document-type';
+import { DocumentTypeService } from '../document-type.service';
+import { GrowlMessageService } from '../../shared/growl-message/growl-message.service';
+
+@Component({
+  selector: 'rb-document-type-wall',
+  templateUrl: 'document-type-wall.component.html',
+  styleUrls: ['./document-type-wall.component.scss']
+})
+export class DocumentTypeWallComponent implements OnInit {
+
+  documentTypes: DocumentType[];
+
+  selectedDocumentType: DocumentType;
+
+  displayDialog: boolean;
+
+  constructor(private documentTypeService: DocumentTypeService) { }
+
+  ngOnInit() {
+    this.documentTypeService.fetchData();
+    this.documentTypeService.documentTypesChanged.subscribe(
+      (documentTypes: DocumentType[]) => this.documentTypes = documentTypes
+    );
+  }
+
+  selectDocumentType(documentType: DocumentType) {
+    this.selectedDocumentType = documentType;
+    this.displayDialog = true;
+  }
+
+  onDialogHide() {
+    this.selectedDocumentType = null;
+  }
+}
--- src/app/document-type/document-type.module.ts
+++ src/app/document-type/document-type.module.ts
@@ -5,19 +5,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PanelModule } from 'primeng/components/panel/panel';
 import { AutoCompleteModule } from 'primeng/components/autocomplete/autocomplete';
 import { DataTableModule } from 'primeng/components/datatable/datatable';
+import { DataGridModule } from 'primeng/components/datagrid/datagrid';
+import { DialogModule } from 'primeng/components/dialog/dialog';
+import { PaginatorModule } from 'primeng/components/paginator/paginator';
 import { TabViewModule } from 'primeng/components/tabview/tabview';
 import { CarouselModule } from 'primeng/components/carousel/carousel';
 import { GrowlModule } from 'primeng/components/growl/growl';
 
 import { DocumentTypeListComponent } from './document-type-list/document-type-list.component';
 import { DocumentTypeItemComponent } from './document-type-item/document-type-item.component';
+import { DocumentTypeWallComponent } from './document-type-wall/document-type-wall.component';
 
 @NgModule({
     declarations: [
         DocumentTypeListComponent,
-        DocumentTypeItemComponent
+        DocumentTypeItemComponent,
+        DocumentTypeWallComponent
     ],
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, PanelModule, AutoCompleteModule, DataTableModule, TabViewModule, CarouselModule, GrowlModule],
-    exports: [DocumentTypeListComponent]
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, PanelModule, AutoCompleteModule, DataTableModule, DataGridModule, DialogModule, PaginatorModule, TabViewModule, CarouselModule, GrowlModule],
+    exports: [DocumentTypeListComponent, DocumentTypeWallComponent]
 })
 export class DocumentTypeModule { }
